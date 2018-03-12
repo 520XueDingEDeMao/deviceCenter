94ac868a48a5ebac942e286d9ec1571428801e54
ningque <ningque@dian.so>
2018-03-12 20:40:07 +0800

    修改配置

--- src/main/java/com/device/config/prop/AliGeniceClientProperties.java
+++ src/main/java/com/device/config/prop/AliGeniceClientProperties.java
@@ -4,28 +4,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 /**
  * 天猫精灵的client配置
  * @author ningque
  *
  */
 @Configuration
-@ConfigurationProperties(prefix="auth.aligenie.client")
+@ConfigurationProperties(prefix="oauth.aligenie.client")
 public @Data class AliGeniceClientProperties {
 	
-	@Getter@Setter
 	private String client_id;
-	@Getter@Setter
 	private String client_secret;
-	@Getter@Setter
 	private String authorizedGrantTypes;
-	@Getter@Setter
 	private String scopes;
-	@Getter@Setter
 	private int accessTokenValiditySeconds;
-	@Getter@Setter
 	private int refreshTokenValiditySeconds;
 
 }
